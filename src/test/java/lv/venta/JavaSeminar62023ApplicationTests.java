3dfe7b9e0f32a2cc7f064de8cabb8a4195ace12d
localuser <localuser@VEA.LV>
2023-05-18 13:20:21 +0300

    Add: new project for Seminar 6

---
+++ src/test/java/lv/venta/JavaSeminar62023ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeminar62023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

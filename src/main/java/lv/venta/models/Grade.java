3ae85a287fe89ca2a53582a1803ce178f0214dc0
localuser <localuser@VEA.LV>
2023-05-18 14:19:36 +0300

    Add: new model class real Grade

---
+++ src/main/java/lv/venta/models/Grade.java
@@ -0,0 +1,36 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "grade_table")//table in DB
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Grade {
+	@Column(name = "Idg")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idg;
+	
+	@Column(name="Gvalue")
+	@Min(value = 0)
+	@Max(value = 10)
+	private int gvalue;
+	
+	//TODO add constructor later
+}

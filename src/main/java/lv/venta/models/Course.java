4a4875066ae652ff51df2c86be81a3ff9aef1771
localuser <localuser@VEA.LV>
2023-05-18 14:14:46 +0300

    Add: new model class Grade

---
+++ src/main/java/lv/venta/models/Course.java
@@ -0,0 +1,46 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "course_table")//table in DB
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Course {
+	
+	@Column(name = "Idc")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idc;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
+	@Column(name="Title")
+	private String title;
+	
+	@Column(name="Creditpoints")
+	@Min(value = 1)
+	@Max(value = 20)
+	private int creditpoints;
+	
+	//TODO add constructor later
+}

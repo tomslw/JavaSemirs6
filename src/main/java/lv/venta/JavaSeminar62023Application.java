3afd2adecb75d6a932bbd6c09f230dd86356f583
localuser <localuser@VEA.LV>
2023-05-18 15:29:01 +0300

    Add: model testing function

--- src/main/java/lv/venta/JavaSeminar62023Application.java
+++ src/main/java/lv/venta/JavaSeminar62023Application.java
@@ -1,13 +1,62 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.models.Course;
+import lv.venta.models.Degree;
+import lv.venta.models.Grade;
+import lv.venta.models.Professor;
+import lv.venta.models.Student;
+import lv.venta.repos.ICourseRepo;
+import lv.venta.repos.IGradeRepo;
+import lv.venta.repos.IProfessorRepo;
+import lv.venta.repos.IStudentRepo;
 
 @SpringBootApplication
 public class JavaSeminar62023Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSeminar62023Application.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner testModel(IProfessorRepo prRepo, 
+			IStudentRepo stRepo, ICourseRepo coRepo, IGradeRepo grRepo) {
+		
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+			
+				
+				Professor p1 = new Professor("Juris", "Zagars", Degree.phd);
+				Professor p2 = new Professor("Dmitrijs", "Smirnovs", Degree.phd);
+				prRepo.save(p1);
+				prRepo.save(p2);
+				
+				Student s1 = new Student("Janis", "Berzins");
+				Student s2 = new Student("Baiba", "Kalnina");
+				stRepo.save(s1);
+				stRepo.save(s2);
+				
+				Course c1 = new Course("Haosa teorija", 4, p1);
+				Course c2 = new Course("Ekonimikas pamati", 2, p2);
+				coRepo.save(c1);
+				coRepo.save(c2);
+				
+				grRepo.save(new Grade(5, s1, c1));//Janis got 5 in Haoss
+				grRepo.save(new Grade(7, s1, c2));//Janis got 7 in Ekonomika
+				grRepo.save(new Grade(10, s2, c1));//Baiba got 10 in Haoss
+				grRepo.save(new Grade(8, s2, c2));//Baiba got 8 in Ekonomika
+				
+			}
+		};
+		
+		
 	}
 
 }
